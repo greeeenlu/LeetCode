0e487224e47e7bd41128d263509ec66bfb462c51
GreenLu <green.ee02g@g2.nctu.edu.tw>
2020-07-03 23:26:42 +0800

    p0001 >>> print test case

--- Algorithms/0001-0025/p0001.py
+++ Algorithms/0001-0025/p0001.py
@@ -13,3 +13,10 @@ class Solution:
                 return numsDict[i][:2]
             elif temp != i and temp in numsDict:
                 return numsDict[i][:1] + numsDict[temp][:1]
+
+
+print(Solution().twoSum([2, 7, 11, 15], 9))
+print(Solution().twoSum([3, 3], 6))
+print(Solution().twoSum([2, 5, 5, 11], 10))
+print(Solution().twoSum([2, 5, 11, 5], 10))
+print(Solution().twoSum([3, 2, 4], 6))
